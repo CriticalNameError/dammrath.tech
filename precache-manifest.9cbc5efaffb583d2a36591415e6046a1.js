d2cdefd41413ae80cfbcd7d6a1dbab0bd21eacc0
Martin Dammrath <martin.dammrath@gmail.com>
2020-09-02 08:07:41 +0200

    Updates

---
+++ precache-manifest.9cbc5efaffb583d2a36591415e6046a1.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "b4ea855e5877ee403370",
+    "url": "/dammrath.tech/static/js/runtime~main.488ab52b.js"
+  },
+  {
+    "revision": "70aa1cc1921938ad1cc3",
+    "url": "/dammrath.tech/static/js/main.cf104e6c.chunk.js"
+  },
+  {
+    "revision": "9306d7133b673b4f4746",
+    "url": "/dammrath.tech/static/js/2.ec176464.chunk.js"
+  },
+  {
+    "revision": "70aa1cc1921938ad1cc3",
+    "url": "/dammrath.tech/static/css/main.07318036.chunk.css"
+  },
+  {
+    "revision": "65fbf85a8a245c4b98b44530d909feb3",
+    "url": "/dammrath.tech/index.html"
+  }
+];
