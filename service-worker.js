f294d25044f28c5e1c4b3e52f5fb311914bc9d2f
Martin Dammrath <martin.dammrath@gmail.com>
2020-09-02 08:22:07 +0200

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/dammrath.tech/precache-manifest.9cbc5efaffb583d2a36591415e6046a1.js"
+  "./precache-manifest.f04684c7a4c2caf672017ed25e3569bf.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/dammrath.tech/index.html", {
+workbox.routing.registerNavigationRoute("./index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
